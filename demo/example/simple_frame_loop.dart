69ed62eea4ab4235b32612ca7c462a91b37ebbb1
Aske Simon Christensen <askesc@gmail.com>
2025-01-19 13:27:20 +0100

    Simple frame loop example

---
+++ demo/example/simple_frame_loop.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+import 'package:no_cpu/memory.dart';
+import 'package:no_cpu/copper.dart';
+import 'package:no_cpu/custom.dart';
+
+main() {
+  var m = Memory(0x20_0000);
+  List<Copper> frames = List.generate(16, (i) => m.copper(origin: i));
+  for (var (i, frame) in frames.indexed) {
+    frame.move(COLOR00, 0x005);
+    frame.wait(v: 100, h: 7);
+    frame.move(COLOR00, i * 0x111);
+    frame.ptr(COP1LC, frames[(i + 1) % frames.length].label);
+    frame.end();
+  }
+
+  Copper initialCopper = m.copper(origin: "Initial")
+    ..data.address = 0x00_0000;
+  initialCopper.ptr(COP1LC, frames[0].label);
+  initialCopper.end();
+
+  File("../runner/chip.dat").writeAsBytesSync(m.finalize()); 
+}
