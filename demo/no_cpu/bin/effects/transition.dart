e481c3e178b2d6537a91c7352b3a94febf0bc5f3
Aske Simon Christensen <askesc@gmail.com>
2025-03-11 12:01:18 +0100

    Transition effect

---
+++ demo/no_cpu/bin/effects/transition.dart
@@ -0,0 +1,76 @@
+import 'package:no_cpu/no_cpu.dart';
+
+class Transition {
+  final Bitmap pattern;
+  late Bitmap temp = Bitmap.space(pattern.width, pattern.height, 1);
+  late Bitmap result = Bitmap.space(pattern.width, pattern.height, 1);
+
+  Transition(this.pattern) : assert(pattern.depth == 7);
+
+  Transition.generate(
+    int width,
+    int height,
+    int Function(int x, int y) generator,
+  ) : this(
+        Bitmap.fromChunky(
+          ChunkyPixels.generate(
+            width,
+            height,
+            (x, y) => generator(x, y).clamp(0, 127),
+          ),
+          depth: 7,
+        ),
+      );
+
+  Transition.fromIlbm(IlbmImage image) : this(Bitmap.fromIlbm(image));
+
+  Transition.fromFile(String path) : this.fromIlbm(IlbmImage.fromFile(path));
+
+  /// Set bits in the result where the corresponding pixel in the pattern is
+  /// greater or equal to (less than, if [inverse] is true) the [threshold].
+  CopperComponent run(int threshold, {bool inverse = false}) {
+    assert(threshold >= 0 && threshold <= 128);
+    return TransitionRun(this, threshold, inverse);
+  }
+}
+
+class TransitionRun implements CopperComponent {
+  final Transition transition;
+  final int threshold;
+  final bool inverse;
+
+  TransitionRun(this.transition, this.threshold, this.inverse);
+
+  Bitmap get pattern => transition.pattern;
+  Bitmap get temp => transition.temp;
+  Bitmap get result => transition.result;
+
+  @override
+  void addToCopper(Copper copper) {
+    var pass1 =
+        Blit()
+          ..aSetBitplane(pattern, 2)
+          ..bSetBitplane(pattern, 1)
+          ..cSetBitplane(pattern, 0)
+          ..dSetBitplane(temp, 0)
+          ..minterms = (-1 << (threshold & 7)) & 0xFF;
+
+    var pass2 =
+        Blit()
+          ..aSetBitplane(pattern, 4)
+          ..bSetBitplane(pattern, 3)
+          ..cdSetBitplane(temp, 0)
+          ..minterms = (-2 << ((threshold >> 2) & 6)) & 0xFF;
+
+    var pass3 =
+        Blit()
+          ..aSetBitplane(pattern, 6)
+          ..bSetBitplane(pattern, 5)
+          ..cSetBitplane(temp, 0)
+          ..dSetBitplane(result, 0)
+          ..minterms =
+              ((-2 << ((threshold >> 4) & 14)) & 0xFF) ^ (inverse ? 0xFF : 0);
+
+    copper << pass1 << pass2 << pass3;
+  }
+}
---
+++ demo/no_cpu/bin/transition_test.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+import 'dart:math';
+
+import 'package:no_cpu/no_cpu.dart';
+
+import 'effects/transition.dart';
+
+main() {
+  var trans1 = Transition.generate(320, 180, (x, y) {
+    var dx = x - 80;
+    var dy = y - 80;
+    return (20 +
+            sqrt(dx * dx + dy * dy) * 0.28 +
+            (cos(x * 0.3) + cos(y * 0.3)) * 10)
+        .toInt();
+  });
+  var trans2 = Transition.generate(
+    320,
+    180,
+    (x, y) =>
+        (20 +
+                x * (0.2 + y * 0.001) -
+                y * 0.1 +
+                (sin(y * 0.11 + x * 0.013) + sin(y * 0.13 - x * 0.015)) * 5)
+            .toInt(),
+  );
+  var result = trans1.result = trans2.result;
+
+  List<Copper> frames = List.generate(
+    256,
+    (i) => Copper(isPrimary: true, origin: i)..useInFrame(i),
+  );
+  for (var (i, frame) in frames.indexed) {
+    frame >> (Display()..setBitmap(result));
+    frame.wait(v: 0x54);
+    if (i < 128) {
+      frame << trans1.run(i, inverse: true);
+    } else {
+      frame << trans2.run(256 - i, inverse: true);
+    }
+    frame.ptr(COP1LC, frames[(i + 1) % frames.length].label);
+  }
+
+  Copper initialCopper =
+      Copper(isPrimary: true, origin: "Initial")
+        ..data.address = 0x00_0000
+        ..useInFrame(-1);
+  initialCopper.move(DIWSTRT, 0x5281);
+  initialCopper.move(DIWSTOP, 0x06C1);
+  initialCopper.move(BPLCON3, 0x0020);
+  initialCopper.move(COLOR00, 0x205);
+  initialCopper.move(COLOR01, 0xa85);
+  initialCopper.ptr(COP1LC, frames[0].label);
+
+  Memory m = Memory.fromRoots(0x20_0000, [initialCopper.data]);
+  File("../runner/chip.dat").writeAsBytesSync(m.build());
+}
