a3fa32d6d4ea61fc7c43a9ce678b4a9645806990
Aske Simon Christensen <askesc@gmail.com>
2025-08-02 22:04:26 +0200

    Continual noise in the middle of Game of Life

--- no_cpu/example/game_of_life_test.dart
+++ no_cpu/example/game_of_life_test.dart
@@ -12,24 +12,28 @@ main() {
 class GameOfLifeTest extends DemoBase {
   GameOfLife gameOfLife = GameOfLife(336, 180, 16, 0);
 
-  GameOfLifeTest() : super(2, loopFrame: 0) {
+  GameOfLifeTest() : super(6, loopFrame: 0) {
     Random random = Random(DateTime.now().millisecondsSinceEpoch);
-    Bitmap bitmap = Bitmap.generate(
-      336,
-      180,
-      (x, y) => x >= 160 - 32 && x < 160 + 32 && y >= 90 - 32 && y < 90 + 32
-          ? random.nextInt(2)
-          : 0,
+    Bitmap center = Bitmap.generate(
+      32,
+      32,
       depth: 1,
-      mutability: Mutability.mutable,
+      (x, y) => random.nextInt(2),
     );
+    Bitmap bitmap = Bitmap.space(336, 180, 1);
+    initialCopper << (Blit()..dSetBitplane(bitmap, 0));
+
     Display display = Display()..setBitmap(bitmap);
     Palette palette = Palette.rgb12([0x025, 0x8af]);
 
     var blits = gameOfLife.step(bitmap);
-    F(0, 0) - 1 << display << palette;
-    F(0, 0, 0) << (blits.sublist(13) + blits.sublist(0, 9));
-    F(0, 0, 1) << blits.sublist(9, 13) |
+    F(0, 0) - 5 << display << palette;
+    F(0, 0, 0) <<
+        (Blit()
+          ..aSetBitplane(center, 0)
+          ..cdSetBitplane(bitmap, 0, x: 160 - 16, y: 90 - 16, w: 32, h: 32));
+    F(0, 0, 0) + 2 + 2 << (blits.sublist(13) + blits.sublist(0, 9));
+    F(0, 0, 1) + 2 + 2 << blits.sublist(9, 13) |
         (i, c) {
           c.waitBlit();
           c.move(BPLCON3, 0X0000);
